e898ebe4a9718ae5a068546390a5bebb62bed99a
Michael <michaelbrunauer@yahoo.de>
2020-03-31 02:14:41 +0200

    First draf of Exercise 1.7 added. Might improve on it later

--- Exercise1.rkt
+++ Exercise1.rkt
@@ -19,3 +19,64 @@
   (if (= x 0)
       0
       y))
+
+;Excercise 1.6
+;Newton method taken from SICP
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess)x)) 0.001))
+(define (abs x)
+  (if (< x 0)
+      (* x -1)
+      x))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+;Using cond based if for sqrt-iter
+(define (sqrt-iter2 guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter2 (improve guess x)
+                     x)))
+(define (sqrt2 x)
+  (sqrt-iter2 1.0 x))
+;The program does't terminate. Because new-if is not a special form all operands are expanded
+;before starting evaluation. Thus the else clause of new-if is also evaluated even if the
+; predicate is #t. In this case this causes an endless recursion.
+
+;Exercise 1.7
+(sqrt 8100000000)
+(sqrt (/ 1 10000))
+(* (/ 0.001 8100000000) 100) ;off by 1.2 * 10^(-11) % Uses additional iterations for mostly unnecessary precise approximation
+(* (/ 0.001 (/ 1 10000)) 100) ; off by 1000% Stop way before any precie approximation is reached
+
+(define (pcent-change x y)
+  (cond ((> x y) (/ y x))
+        (else (/ x y))))
+
+(define (better-good-enough? guess x)
+   (< (abs (- (square guess) x)) (* 0.001 x)))
+
+(define (sqrt-iter3 guess x)
+  (if (better-good-enough? guess x)
+      guess
+      (sqrt-iter3 (improve guess x) x)))
+
+(define (sqrt3 x)
+  (sqrt-iter3 1.0 x))
+5
+(sqrt3 8100000000)
+(sqrt3 (/ 1 10000))
+      
